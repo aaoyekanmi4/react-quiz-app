aa8b438d8d83b479abc7a94ad4824c8991eccd91
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-02-14 17:59:16 -0600

    Added startscreen

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Question from '../Question/Question';
+import EndScreen from '../EndScreen/EndScreen';
+import StartScreen from '../StartScreen/StartScreen';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <StartScreen />
+    {/* <Question />
+    <EndScreen /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StartScreen/StartScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './StartScreen.css';
+const StartScreen = () => {
+  return (
+    <section className="start-quiz">
+      <div className="start-text">
+        <h1> JavaScript Array Quiz</h1>
+
+        <button className="start">Start Quiz</button>
+      </div>
+    </section>
+  );
+};
+
+export default StartScreen;
